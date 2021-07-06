7d7cf6fbd42cc1f561ce52440e6b8046a8398ba8
derekli19951103 <derekli19951103@gmail.com>
2021-07-06 19:05:07 -0400

    jul 6

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -16,7 +16,7 @@
 
 use std::collections::HashMap;
 
-#[derive(Hash, PartialEq, Eq)]
+#[derive(Hash, PartialEq, Eq, Debug)]
 enum Fruit {
     Apple,
     Banana,
@@ -38,6 +38,8 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        println!("{:?}", fruit);
+        basket.entry(fruit).or_insert(2);
     }
 }
 
